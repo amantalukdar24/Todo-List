53e0f233c4a4124e61e3b0f25ac0cd6b9d98d5b1
amantalukdar24 <amantalukdar24@gmail.com>
2025-03-16 18:13:51 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const app=express();
+mongoose.connect("mongodb://127.0.0.1:27017/todo")
+        .then(()=>{
+            console.log("Mongo Connected");})
+        .catch(err=>console.log(`Mongo error ${err}`));
+const todoSchema=new mongoose.Schema({
+    
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+    },
+});
+const Todo=mongoose.model('todo',todoSchema);
+
+
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/',async (req,res)=>{
+ const allTodo=await Todo.find({});
+  const html=`
+  <html>
+  <h1><b>Todo List</b></h1>
+  <ol>
+       ${ 
+        allTodo.map((ele)=>{
+            if(ele.description!="")
+            {
+              return `<li><div><h3>${ele.title}--(Task_id:${ele._id})</h3></div><div>➡️${ele.description}</div></li></br>`
+            }
+            else{
+                return `<li><div>${ele.title}</li></div></br>`
+            }
+        
+        }).join('')
+       }
+  </ol>
+  </html>
+
+  `;
+  return res.send(html);
+});
+app.post('/addTodo',async (req,res)=>{
+    await Todo.create({
+        title:req.body.title,
+        description:req.body.description,
+    });
+    
+        return res.status(201).json({newTodo:"Todo Added successfully"});
+    
+   
+   
+});
+app.patch('/updateTodo/:id',async (req,res)=>{
+    await Todo.findByIdAndUpdate(req.params.id,{
+         title:req.body.title,
+         description:req.body.description,
+    });
+    return res.send({updated:"updated succesfully"});
+});
+app.delete("/deleteTodo/:id",async (req,res)=>{
+    await Todo.findByIdAndDelete(req.params.id);
+    return res.send({delete:" Todo deleted sucessfully"});
+})
+app.listen(3000,()=>{
+    console.log(`Server is running on the port 3000`);
+});
